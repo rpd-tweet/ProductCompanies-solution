8bf7fc858a97f0f46ef0bb0e34aa632eb7c64df4
rpdstranger <60440808+rpd-tweet@users.noreply.github.com>
2020-02-14 21:35:37 +0530

    Add files via upload

---
+++ UniqueCharacters.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class UniqueCharacters {
+  String string_to_check;
+
+  public UniqueCharacters() // constructor
+  {
+    string_to_check = "";
+  }
+
+  boolean isUnique() {
+    int check = 0;
+    for (int i = 0; i < string_to_check.length(); i++) {
+      char ch=string_to_check.charAt(i);
+	  if(string_to_check.indexOf(ch)!=string_to_check.lastIndexOf(ch))
+	  {
+		  return false;
+      }
+	}
+    return true;
+
+  }
+  
+
+  public static void main(String[] args) {
+
+    UniqueCharacters uniquecharacters = new UniqueCharacters();
+    Scanner sc = new Scanner(System.in);
+    uniquecharacters.string_to_check = sc.next();
+    if (uniquecharacters.isUnique()) {
+      System.out.println("String has unique characters");
+    } else {
+      System.out.println("String has duplicate characters");
+    }
+
+  }
+}
