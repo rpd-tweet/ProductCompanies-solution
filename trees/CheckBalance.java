1fcf8596e4834a4d4de55f4510ae9ea6cbd6ff0e
rpdstranger <60440808+rpd-tweet@users.noreply.github.com>
2020-03-11 16:31:45 +0530

    Add files via upload

---
+++ trees/CheckBalance.java
@@ -0,0 +1,110 @@
+import java.util.*;
+class Node
+{
+	Node left,right;
+	int data;
+	Node(int d)
+	{
+		this.data=d;
+		this.left=this.right=null;
+	}
+}
+class height
+	{
+		int height=0;
+	}
+class CheckBalance
+{
+	Node root;
+	
+	
+	void insert(String s,int d)
+	{
+		
+		Node temp=this.root;
+		if (temp==null)
+		{
+			this.root=new Node(d);
+			return;
+		}
+		for(int i=0;i<s.length();i++)
+		{
+			if(s.charAt(i)=='L')
+			{
+				if (temp.left==null)
+				{
+					temp.left=new Node(d);
+					return;
+				}
+				temp=temp.left;
+			}
+			if(s.charAt(i)=='R')
+			{
+				if (temp.right==null)
+				{
+					temp.right=new Node(d);
+					return;
+				}
+				temp=temp.right;
+			}
+		}
+	}
+	
+	
+	boolean checkBalance(Node root,height height)
+	{
+		if (root==null)
+		{
+			height.height=0;
+			return false;
+		}
+		height lh=new height();
+		height rh=new height();
+		boolean left=checkBalance(root.left,lh);
+		boolean right=checkBalance(root.right,rh);
+		
+		height.height=(lh.height>rh.height?lh.height:rh.height)+1;
+		
+		if (lh.height-rh.height>=2 || rh.height-lh.height>=2)
+		{
+			return false;
+		}
+		return left&&right;
+		
+	}
+	
+	
+	
+	
+		
+	void print(Node root)
+	{
+		if (root==null)
+			return;
+		print(root.left);
+		System.out.print(root.data+" ");
+		print(root.right);
+	}
+	
+	public static void main(String rpd[])
+	{
+		CheckBalance checkbalance=new CheckBalance();
+		height height=new height();
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Size :");
+		int size=sc.nextInt();
+		for(int i=0;i<size;i++)
+		{
+			checkbalance.insert(sc.next(),sc.nextInt());
+		}
+		if(checkbalance.checkBalance(checkbalance.root,height))
+		{
+			System.out.println("Balanced..");
+		}
+		else
+		{
+			System.out.println("Not Balanced..");
+		}
+		checkbalance.print(checkbalance.root);
+	}
+}
